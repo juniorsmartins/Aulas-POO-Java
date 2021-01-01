d096c44f446f2af21367a66980fafbe3082e3bbb
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-01 09:17:01 -0400

    Treino

--- Aulas-POO-Java/src/AulasGustavoGuanabara/VetorI.java
+++ Aulas-POO-Java/src/AulasGustavoGuanabara/VetorI.java
@@ -1,5 +1,24 @@
 package AulasGustavoGuanabara;
 
-public class VetorI {
+import java.util.Scanner;
+
+public class VetorI 
+{    
     
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String[] calendario = new String[12];
+
+        for (byte mes = 0; mes < 12; mes++)
+        {
+            System.out.format("Nome do mês %d: ", (mes+1));
+            calendario[mes] = input.nextLine();
+        }
+        
+        for (String item : calendario)
+        {
+            System.out.println("Mês: " + item);
+        }
+    }
 }
