f95a011cc5fe8232a6cd0fe4c18fa5dbdbe551c2
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-03 14:41:12 -0400

    Treino

--- Aulas-POO-Java/src/AulasOutros/FabricaCarros.java
+++ Aulas-POO-Java/src/AulasOutros/FabricaCarros.java
@@ -1,29 +1,17 @@
 package AulasOutros;
 
-import java.util.Vector;
-
 public class FabricaCarros {
 
     public static void main(String[] args)
     {
         
-     Vector carro = new Vector(10, 10);
-     String marquinha = new String();
-     int potencinha;
-     int portinhas;
-     int contador = 0;
-     String continuar = new String();
-
-     do 
-     {         
-
-         ProjetoCarro carro[contador] = new ProjetoCarro(marquinha, potencinha, portinhas);
-         
+        ProjetoCarro carro = new ProjetoCarro("Saveiro", 2, 2);
         
-     
-     } while(!"S".equals(continuar));
+        carro.statusCarro();
 
-     
+        carro.setLigarCarro();
+        
+        carro.statusCarro();
 
         
     }
