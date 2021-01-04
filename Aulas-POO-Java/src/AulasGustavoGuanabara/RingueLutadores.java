565224cc35382a09b420be1438394b79f922317a
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-04 19:48:59 -0400

    Treino

---
+++ Aulas-POO-Java/src/AulasGustavoGuanabara/RingueLutadores.java
@@ -0,0 +1,21 @@
+package AulasGustavoGuanabara;
+
+
+public class RingueLutadores {
+    
+    public static void main(String[] args)
+    {
+        ClasseLutador lutador1 = new ClasseLutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1);
+        
+        ClasseLutador lutador2 = new ClasseLutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+        
+        ClasseLutador lutador3 = new ClasseLutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+        
+        ClasseLutador lutador4 = new ClasseLutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+        
+        ClasseLutador lutador5 = new ClasseLutador("Ufocobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+        
+        ClasseLutador lutador6 = new ClasseLutador("Nerdaard", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+    }
+    
+}
