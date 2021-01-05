d2f2dbcb053fcae712acfccc126763ffd6c6353a
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-04 20:10:34 -0400

    Treino

---
+++ Aulas-POO-Java/src/AulasGustavoGuanabara/Luta.java
@@ -0,0 +1,64 @@
+package AulasGustavoGuanabara;
+
+
+public class Luta {
+    
+    // Atributos de Instância
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private boolean aprovada;
+    
+    // Método Construtor
+    public Luta()
+    {
+        
+    }
+    
+    // Métodos Personalizados
+    public void marcarLuta()
+    {
+        
+    }
+    public void lutar()
+    {
+        
+    }
+    
+    // Métodos Setters e Getters
+    protected void setDesafiado(String fiado)
+    {
+        this.desafiado = fiado;
+    }
+    protected String getDesafiado()
+    {
+        return this.desafiado;
+    }
+    
+    protected void setDesafiante(String fiante)
+    {
+        this.desafiante = fiante;
+    }
+    protected String getDesafiante()
+    {
+        return this.desafiante;
+    }
+    
+    protected void setRounds(int roun)
+    {
+        this.rounds = roun;
+    }
+    protected int getRounds()
+    {
+        return this.rounds;
+    }
+    
+    protected void setAprovada()
+    {
+        
+    }
+    protected boolean getAprovada()
+    {
+        
+    }
+}
