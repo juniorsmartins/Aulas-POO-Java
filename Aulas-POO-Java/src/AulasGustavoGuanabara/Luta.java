71c99a3cac10f250a8726ecbd9480e090a606174
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-06 17:51:04 -0400

    Treino

--- Aulas-POO-Java/src/AulasGustavoGuanabara/Luta.java
+++ Aulas-POO-Java/src/AulasGustavoGuanabara/Luta.java
@@ -1,11 +1,12 @@
 package AulasGustavoGuanabara;
 
+import java.util.Random;
 
 public class Luta {
     
     // Atributos de Instância
-    private String desafiado;
-    private String desafiante;
+    private ClasseLutador desafiado;
+    private ClasseLutador desafiante;
     private int rounds;
     private boolean aprovada;
     
@@ -16,30 +17,75 @@ public class Luta {
     }
     
     // Métodos Personalizados
-    public void marcarLuta()
+    public void marcarLuta(ClasseLutador lutador1, ClasseLutador lutador2)
     {
-        
+        if (lutador1.getCategoriaLutador().equals(lutador2.getCategoriaLutador()) && lutador1 != lutador2)
+        {
+            this.aprovada = true;
+            this.desafiado = lutador1;
+            this.desafiante = lutador2;
+        }
+        else
+        {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
     }
     public void lutar()
     {
-        
+     if (this.aprovada)
+     {
+         System.out.println("----------------------");
+         System.out.println("Desafiado");
+         desafiado.apresentarLutador();
+         desafiado.statusLutador();
+         System.out.println("----------------------");         
+         System.out.println("Desafiante");
+         desafiante.apresentarLutador();
+         desafiante.statusLutador();
+         Random aleatorio = new Random();
+         int vencedor = aleatorio.nextInt(3);
+         switch(vencedor)
+         {
+             case 0:
+                 System.out.println("\n\nEmpate!");
+                 this.desafiado.empatarLuta();
+                 this.desafiante.empatarLuta();
+                 break;
+             case 1:
+                 System.out.println("\n\nO desafiado: " + this.desafiado.getNomeLutador() + " venceu!");
+                 this.desafiado.ganharLuta();
+                 this.desafiante.perderLuta();
+                 break;
+             case 2:
+                 System.out.println("\n\nO desafiante: " + this.desafiante.getNomeLutador() + " venceu!");
+                 this.desafiante.ganharLuta();
+                 this.desafiado.perderLuta();
+                 break;
+         }
+     }
+     else
+     {
+         System.out.println("Luta desaprovada!");
+     }
     }
     
     // Métodos Setters e Getters
-    protected void setDesafiado(String fiado)
+    protected void setDesafiado(ClasseLutador fiado)
     {
         this.desafiado = fiado;
     }
-    protected String getDesafiado()
+    protected ClasseLutador getDesafiado()
     {
         return this.desafiado;
     }
     
-    protected void setDesafiante(String fiante)
+    protected void setDesafiante(ClasseLutador fiante)
     {
         this.desafiante = fiante;
     }
-    protected String getDesafiante()
+    protected ClasseLutador getDesafiante()
     {
         return this.desafiante;
     }
@@ -53,12 +99,12 @@ public class Luta {
         return this.rounds;
     }
     
-    protected void setAprovada()
+    protected void setAprovada(boolean aprov)
     {
-        
+        this.aprovada = aprov;
     }
     protected boolean getAprovada()
     {
-        
+        return this.aprovada;
     }
 }
--- Aulas-POO-Java/src/AulasGustavoGuanabara/RingueLutadores.java
+++ Aulas-POO-Java/src/AulasGustavoGuanabara/RingueLutadores.java
@@ -8,23 +8,19 @@ public class RingueLutadores {
         ClasseLutador lutadores[] = new ClasseLutador[6];
         
         lutadores[0] = new ClasseLutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
-        
         lutadores[1] = new ClasseLutador("Putscript", "Brasil", 29, (float) 1.68, (float) 57.8, 14, 2, 3);
-        
         lutadores[2] = new ClasseLutador("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
-        
         lutadores[3] = new ClasseLutador("Dead Code", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
-        
         lutadores[4] = new ClasseLutador("Ufocobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
-        
         lutadores[5] = new ClasseLutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
         
-        for (byte contador = 0; contador < 6; contador++)
-        {
-            lutadores[contador].apresentarLutador();
-            lutadores[contador].statusLutador();
-        }
 
+
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(lutadores[0], lutadores[1]);
+        UEC01.lutar();
+        lutadores[0].statusLutador();
+        lutadores[1].statusLutador();
         
         
     }
