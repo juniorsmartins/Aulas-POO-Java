32f0527eb7a971a80144907280a7ba969a9b9f0a
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-05 20:57:27 -0400

    Treino

---
+++ Aulas-POO-Java/src/AulasLoianeGroner/RelacionalOperador.java
@@ -0,0 +1,18 @@
+package AulasLoianeGroner;
+
+public class RelacionalOperador {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(" " + (1 > 0));
+        System.out.println(" " + (2 < 3));
+        System.out.println(" " + (4 != 4));
+        System.out.println(" " + (5 == 4));
+        System.out.println(" " + (5 == 5));
+        System.out.println(" " + (6 <= 7));
+        System.out.println(" " + (5 >= 10));
+        System.out.println(" " + (6 <= 6));
+    }
+    
+    
+}
