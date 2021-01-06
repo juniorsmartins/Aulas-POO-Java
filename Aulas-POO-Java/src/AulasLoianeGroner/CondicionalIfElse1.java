9ceb813ce519c15c0f910a7ec4a55eeac7350f48
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-06 11:42:30 -0400

    Treino

--- Aulas-POO-Java/src/AulasLoianeGroner/CondicionalIfElse1.java
+++ Aulas-POO-Java/src/AulasLoianeGroner/CondicionalIfElse1.java
@@ -2,16 +2,16 @@ package AulasLoianeGroner;
 
 import java.util.Scanner;
 
-public class CondicionalIf2 {
+public class CondicionalIfElse1 {
 
     public static void main(String[] args)
     {
-        byte idade;
+        short idade;
         
         Scanner teclado = new Scanner(System.in);
         
         System.out.format("Digite idade: ");
-        idade = teclado.nextByte();
+        idade = teclado.nextShort();
         
         System.out.println("\n\bSua idade é: " + idade);
         
@@ -19,6 +19,11 @@ public class CondicionalIf2 {
         {
             System.out.printf("\n\bMaior de Idade!\n\n");
         }
-        
+        else
+        {
+            System.out.println("\nMenor de Idade!\n\n");
+        }
+             
     }
+    
 }
