c2b255a35aadf17f2a316e6b781b79379230f1ac
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-06 11:31:23 -0400

    Treino

---
+++ Aulas-POO-Java/src/AulasLoianeGroner/CondicionalIf1.java
@@ -0,0 +1,19 @@
+package AulasLoianeGroner;
+
+public class CondicionalIf1 {
+
+    public static void main(String[] args)
+    {
+        boolean temBatata = true;
+        int ovos = 6;
+        
+        if (temBatata) {
+            ovos = 9;
+        }
+        
+        System.out.println("Compre " + ovos + " ovos");
+    }
+
+
+    
+}
