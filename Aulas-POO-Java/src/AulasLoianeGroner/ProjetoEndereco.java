2f4bd1e8355cca3c5f570305de22bb83d524248c
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-09 11:17:22 -0400

    Treino

--- Aulas-POO-Java/src/AulasLoianeGroner/ProjetoEndereco.java
+++ Aulas-POO-Java/src/AulasLoianeGroner/ProjetoEndereco.java
@@ -12,12 +12,18 @@ public class ProjetoEndereco {
     private String bairroEndereco;
     private String cidadeEndereco;
     private String estadoEndereco;
-
-
-
     
     /* ---------- ÁREA DE MÉTODOS ---------- */
     // Método Construtor
+    public ProjetoEndereco(String rua, short numero, String bairro, String cidade, String estado)
+    {
+        this.setRuaEndereco(rua);
+        this.setNumeroEndereco(numero);
+        this.setBairroEndereco(bairro);
+        this.setCidadeEndereco(cidade);
+        this.setEstadoEndereco(estado);
+    }
+    
     // Métodos Especiais de Classe
 
     // Métodos Especiais de Instância
@@ -26,9 +32,9 @@ public class ProjetoEndereco {
         System.out.println("--------- Endereço ----------");
         System.out.println("Rua: " + this.getRuaEndereco());
         System.out.println("Número: " + this.getNumeroEndereco());
-        System.out.println("Bairro: ");
-        System.out.println("Cidade: ");
-        System.out.println("Estado: ");
+        System.out.println("Bairro: " + this.getBairroEndereco());
+        System.out.println("Cidade: " + this.getCidadeEndereco());
+        System.out.println("Estado: " + this.getEstadoEndereco());
     }
     
     // Métodos Setters e Getters
@@ -72,5 +78,5 @@ public class ProjetoEndereco {
     public String getEstadoEndereco() {
         return estadoEndereco;
     }
-    
+
 }
