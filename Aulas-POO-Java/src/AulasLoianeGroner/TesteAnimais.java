7cb7a572c15acb78403b7bc101e55760a3902496
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-12 20:52:26 -0400

    Treino

--- Aulas-POO-Java/src/AulasLoianeGroner/TesteAnimais.java
+++ Aulas-POO-Java/src/AulasLoianeGroner/TesteAnimais.java
@@ -14,6 +14,7 @@ public class TesteAnimais
         gato.alimentar();
         cachorro.amamentar();
         papagaio.emitirSom();
+        cachorro.brincar();
         
     }
     
---
+++ Aulas-POO-Java/src/ProjetoDeCrud/MainCrud.java
@@ -0,0 +1,12 @@
+package ProjetoDeCrud;
+
+public class MainCrud 
+{
+    public static void main(String[] args) 
+    {
+
+
+
+        
+    }
+}
