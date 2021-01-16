ecc8d7f36be51275b9807dd1c3630a0d6fafc734
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-16 08:58:59 -0400

    Treino

---
+++ Aulas-POO-Java/src/ProjetoContaBancaria/Controller/Banco.java
@@ -0,0 +1,16 @@
+/*
+ * Exercício para aprendizagem de Java
+ */
+package ProjetoContaBancaria.Controller;
+
+/**
+ * @author Junior Martins
+ * @since 16/01/21 - 08h57
+ */
+public class Banco 
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ Aulas-POO-Java/src/ProjetoContaBancaria/Modelo/Cliente.java
@@ -0,0 +1,74 @@
+/*
+ * Exercício para aprendizagem de Java
+ */
+package ProjetoContaBancaria.Modelo;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author Junior Martins
+ * @since 16/01/21 - 08h34
+ */
+public class Cliente 
+{
+    /* ---------- ÁREA DE ATRIBUTOS ---------- */
+    // Atributos de Classe
+    private static int totalCliente;
+    
+    // Atributos de Instância
+    private String dataCliente;
+    private String idCliente;
+    private String nomeCliente;
+
+    /* ---------- ÁREA DE MÉTODOS ---------- */
+    // Métodos Construtor
+    Cliente(String nome)
+    {
+        Cliente.setTotalCliente();
+        this.setDataCliente();
+        this.setIdCliente();
+        this.nomeCliente = nome;
+    }
+    // Métodos Especiais de Classe
+    // Métodos Especiais de Instância
+
+    // Métodos Getters e Setters
+    public static int getTotalCliente()
+    {
+        return Cliente.totalCliente;
+    }
+    private static void setTotalCliente()
+    {
+        Cliente.totalCliente += 1;
+    }
+
+    public String getDataCliente()
+    {
+        return this.dataCliente;
+    }
+    private void setDataCliente()
+    {
+        Date data = new Date();
+        SimpleDateFormat formatar = new SimpleDateFormat("ddMMyyyy");
+        this.dataCliente = formatar.format(data);
+    }
+
+    public String getIdCliente()
+    {
+        return this.idCliente;
+    }
+    private void setIdCliente()
+    {
+        this.idCliente = (getDataCliente() + "-" + getTotalCliente());
+    }
+
+    public String getNomeCliente()
+    {
+        return this.nomeCliente;
+    }
+    public void setNomeCliente(String nome)
+    {
+        this.nomeCliente = nome;
+    }
+}
