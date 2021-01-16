2f0a31a0623ba844a9e12905c150c7f3a0e1a5f9
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-16 07:15:17 -0400

    Treino

---
+++ Aulas-POO-Java/src/AulasUdemy/ControllerProduto.java
@@ -0,0 +1,17 @@
+package AulasUdemy;
+
+public class ControllerProduto 
+{
+    public static void main(String[] args)
+    {
+        // Declaração de Instância
+        ModeloProduto caneta;
+        
+        // Instanciação de objeto
+        caneta = new ModeloProduto();
+        
+        // Declaração e Instanciação de objeto
+        ModeloProduto carro = new ModeloProduto();
+        
+    }
+}
