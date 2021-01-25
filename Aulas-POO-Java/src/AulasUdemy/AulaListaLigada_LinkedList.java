0213368f8f4dda41f394a8b00f2a815459067281
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-25 11:29:54 -0400

    Treino

--- Aulas-POO-Java/src/AulasUdemy/AulaListaLigada_LinkedList.java
+++ Aulas-POO-Java/src/AulasUdemy/AulaListaLigada_LinkedList.java
@@ -2,8 +2,57 @@ package AulasUdemy;
 
 public class AulaListaLigada_LinkedList 
 {
-    public void adicionaNoComeco(Object elemento)
+    private AulaListaLigada_Celula primeira = null;
+    private int total = 0;
+    
+    public void adicionaNoComeco (Object elemento)
     {
+        AulaListaLigada_Celula nova = new AulaListaLigada_Celula (elemento, this.primeira);
+        this.primeira = nova;
+        this.total = this.total + 1;
+    }
+    
+    public void adiciona (Object elemento)
+    {
+        //TODO
+    }
+    
+    public void adiciona (int posicao, Object elemento)
+    {
+        //TODO
+    }
+
+    public Object pega (int posicao)
+    {
+        //TODO
+        return null;
+    }
+
+    public void remove (int posicao)
+    {
+        //TODO
+    }
+    
+    public int tamanho ()
+    {
+        //TODO
+        return 0;
+    }
+    
+    public boolean contem (Object obj)
+    {
+        //TODO
+        return false;
+    }
+    
+    @Override
+    public String toString ()
+    {
+        if (total == 0) 
+        {
+            return "[]";
+        }
+        return "TODO";
         
     }
 }
--- Aulas-POO-Java/src/AulasUdemy/AulaListaLigada_ProgramaMain.java
+++ Aulas-POO-Java/src/AulasUdemy/AulaListaLigada_ProgramaMain.java
@@ -4,6 +4,7 @@ public class AulaListaLigada_ProgramaMain
 {
     public static void main(String[] args) 
     {
-        
+        AulaListaLigada_LinkedList lista = new AulaListaLigada_LinkedList();
+        System.out.println(lista);
     }
 }
