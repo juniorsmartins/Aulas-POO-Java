d38a7acc5bc208b01968be92530555acf4bfc6cb
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-27 17:11:27 -0400

    Treino

--- Aulas-POO-Java/src/AulasUdemy/AulaFila.java
+++ Aulas-POO-Java/src/AulasUdemy/AulaFila.java
@@ -13,5 +13,25 @@ public class AulaFila
         ClassFila fila = new ClassFila();
         
         System.out.println(fila);
+        
+        fila.adiciona("Angelina");
+        fila.adiciona("Felicity");
+        fila.adiciona("Maria");
+        
+        System.out.println(fila);
+        
+        String removido = fila.remove();
+        System.out.println("O removido foi: " + removido);
+
+        System.out.println("Fila: " + fila);
+        
+        System.out.println("Fila vazia: " + fila.vazia());
+        
+        fila.remove();
+        fila.remove();
+        fila.remove();
+        
+        System.out.println("Fila vazia: " + fila.vazia());
+        System.out.println("Fila: " + fila);
     }
 }
--- Aulas-POO-Java/src/AulasUdemy/ClassFila.java
+++ Aulas-POO-Java/src/AulasUdemy/ClassFila.java
@@ -7,6 +7,30 @@ public class ClassFila
 {
     private List<String> nomes = new LinkedList<String>();
     
+    public void adiciona(String nome) 
+    {
+        this.nomes.add(nome);
+    }
+    
+    public String remove()
+    {
+        try
+        {
+            return nomes.remove(0);            
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            System.out.println("A fila já está vazia!");
+            return "";
+        }
+    }
+    
+    public boolean vazia()
+    {
+        return this.nomes.isEmpty();            
+
+    }
+    
     @Override
     public String toString()
     {
