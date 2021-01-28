63464b6b27117c2593ac351a482769fe5be7f95c
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-28 16:33:54 -0400

    Treino

---
+++ Aulas-POO-Java/src/AulasUdemy/AulaConjuntos4.java
@@ -0,0 +1,16 @@
+package AulasUdemy;
+
+import java.util.LinkedList;
+
+/*
+ * Uma das características dos conjuntos é a não aceitação de elementos repetidos
+*/
+public class AulaConjuntos4 
+{
+    public static void main(String[] args) 
+    {
+        LinkedList<String> conjunto = new LinkedList<String>();
+        
+        System.out.println(conjunto);
+    }
+}
