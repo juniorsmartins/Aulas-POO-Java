2c864928f45a47da6cc9998b92f519a5b93a2e2b
Junior Martins <juniorsoaresmartins@gmail.com>
2021-01-29 17:46:20 -0400

    Treino

---
+++ Aulas-POO-Java/src/AulasUdemy/AulaDefaultMethods.java
@@ -0,0 +1,12 @@
+/*
+ * Aula sobre Default Methods (implementada a partir da versão 8 do Java)
+ */
+package AulasUdemy;
+
+public class AulaDefaultMethods 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
---
+++ Aulas-POO-Java/src/AulasUdemy/InterfaceDefaultMethods.java
@@ -0,0 +1,11 @@
+package AulasUdemy;
+
+
+/*
+ * Aula sobre Default Methods
+ * A partir do Java 8, Interfaces podem contar, além de atributos e métodos abstratos, também com default methods
+*/
+public interface InterfaceDefaultMethods 
+{
+    
+}
