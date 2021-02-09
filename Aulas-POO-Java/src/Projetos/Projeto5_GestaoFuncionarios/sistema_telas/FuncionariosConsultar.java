bd5422b964e758a78fb8ee8df079ac72d617c7a5
Junior Martins <juniorsoaresmartins@gmail.com>
2021-02-09 10:25:41 -0400

    Treino

--- Aulas-POO-Java/src/Projetos/Projeto5_GestaoFuncionarios/sistema_telas/FuncionariosConsultar.java
+++ Aulas-POO-Java/src/Projetos/Projeto5_GestaoFuncionarios/sistema_telas/FuncionariosConsultar.java
@@ -92,8 +92,9 @@ public class FuncionariosConsultar extends JPanel
                 sqlPesquisarFuncionarios(campoFuncionario.getText());
             }
         });
+        
         /*
-         * Ao ser acionado, o botão editar funcionário chamará o método funcionariosEditar da classe Navegador,
+         * Ao ser acionado, o botão Editar Funcionário chamará o método funcionariosEditar da classe Navegador,
          * utilizando como argumento o funcionário selecionado na JList.
         */
         botaoEditar.addActionListener(new ActionListener()
@@ -105,10 +106,82 @@ public class FuncionariosConsultar extends JPanel
             }
         });
         
+        /*
+         * Ao ser acionado, o botão Excluir Funcionário chamará o método sqlDeletarFuncionario
+        */
+        botaoExcluir.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                sqlDeletarFuncionario();
+            }
+        });
+        
+        /*
+         * Ao se selecionar um funcionário na lista, esse funcionário será armazenado na variável funcionarioAtual, e
+         * os botões Editar Cargo e Excluir Cargo serão habilitados.
+        */
+        listaFuncionarios.addListSelectionListener(new ListSelectionListener()
+        {
+            @Override
+            public void valueChanged(ListSelectionEvent e) 
+            {
+                funcionarioAtual = listaFuncionarios.getSelectedValue();
+                if (funcionarioAtual == null)
+                {
+                    botaoEditar.setEnabled(false);
+                    botaoExcluir.setEnabled(false);
+                }
+                else
+                {
+                    botaoEditar.setEnabled(true);
+                    botaoExcluir.setEnabled(true);
+                }
+            }
+        });
     }
     
+    /*
+     * Realiza-se a conexão com o banco de dados para obter todos os funcionários cadastrados no banco, 
+     * de acordo com o nome que foi digitado, para inserir na JList.
+    */
     private void sqlPesquisarFuncionarios(String nome)
     {
-        //TODO
+        // Conexão
+        Connection conexao;
+        // Instrução SQL
+        Statement instrucaoSQL;
+        ResultSet resultados;
+        
+        try
+        {
+            // Conectando ao banco de dados
+            conexao = DriverManager.getConnection(BancoDeDados.stringDeConexao, BancoDeDados.usuario, BancoDeDados.senha);
+            // Criando a instrução SQL
+            instrucaoSQL = conexao.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            resultados = instrucaoSQL.executeQuery("SELECT * FROM funcionarios WHERE nome like '%" + nome + "%' order by nome ASC");
+            
+            listasFuncionariosModelo.clear();
+            
+            while (resultados.next())
+            {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setId(resultados.getInt("id"));
+                funcionario.setNome(resultados.getString("nome"));
+                funcionario.setSobrenome(resultados.getString("sobrenome"));
+                funcionario.setDataNascimento(resultados.getString("dataNascimento"));
+                funcionario.setEmail(resultados.getString("email"));
+                if(resultados.getString("cargo") != null) funcionario.setCargo(Integer.parseInt(resultados.getString("cargo")));
+                funcionario.setSalario(Double.parseDouble(resultados.getString("salario")));
+                
+                listasFuncionariosModelo.addElement(funcionario);
+            }
+        }
+        catch(SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro ao consultar funcionários.");
+            Logger.getLogger(FuncionariosInserir.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
