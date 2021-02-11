8450898989eb7e04aa8c459a976bfd94a05b18d1
Junior Martins <juniorsoaresmartins@gmail.com>
2021-02-10 20:20:14 -0400

    Treino

--- Aulas-POO-Java/src/Hackerrank/Desafio01/Desafio04.java
+++ Aulas-POO-Java/src/Hackerrank/Desafio01/Desafio04.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hackerrank.Desafio01;
 
-/**
- *
- * @author junio
- */
-public class Desafio04 {
-    
+import java.util.Scanner;
+
+public class Desafio04 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int i = Integer.parseInt(scan.nextLine());
+        double d = Double.parseDouble(scan.nextLine());
+        String s = (String)scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+    }
 }
