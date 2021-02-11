098744bbc69bb60c18c67eba864a79fd05c7c71d
Junior Martins <juniorsoaresmartins@gmail.com>
2021-02-10 20:10:34 -0400

    Treino

---
+++ Aulas-POO-Java/src/Hackerrank/Desafio01/Desafio01.java
@@ -0,0 +1,10 @@
+package Hackerrank.Desafio01;
+
+public class Desafio01 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
---
+++ Aulas-POO-Java/src/Hackerrank/Desafio01/Desafio03.java
@@ -0,0 +1,49 @@
+package Hackerrank.Desafio01;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Desafio03 
+{
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) 
+    {
+ 
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        if(N >= 1 && N <= 100)
+        {
+            int resto = (N % 2);
+            if(N >= 2 && N <= 5 && resto == 0)
+            {
+                System.out.println("Not Weird");
+            }
+            else if (N >= 6 && N <= 20 && resto == 0)
+            {
+                System.out.println("Weird");
+            }
+            else if (N > 20 && resto == 0)
+            {
+                System.out.println("Not Weird");
+            }
+            else
+            {
+                System.out.println("Weird");
+            }
+        }
+        else
+        {
+            System.out.println("Valor inválido!");
+        }
+
+        scanner.close();
+    }
+}
