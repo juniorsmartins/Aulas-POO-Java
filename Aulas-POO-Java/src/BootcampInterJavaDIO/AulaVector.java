56fc08cd62234f87af3f1782e58fd3a7636124d4
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-02-13 09:58:00 -0400

    Treino

--- Aulas-POO-Java/src/BootcampInterJavaDIO/AulaVector.java
+++ Aulas-POO-Java/src/BootcampInterJavaDIO/AulaVector.java
@@ -16,11 +16,31 @@ public class AulaVector
         esportes.add("Tênis");
         esportes.add("Futebol Americano");
         esportes.add("Xadrez");
+        esportes.add("Handebol");
         esportes.add("Judô");
         esportes.add("Futsal");
         esportes.add("Vôlei");
         esportes.add("Rugby");
+        esportes.add("Golfe");
+        
+        System.out.println(esportes); // imprime a lista
 
-
+        // Altera o valor da posição e do vector
+        esportes.set(7, "Ping Pong");
+        
+        // Remove o esporte da posição dois
+        esportes.remove(2);
+        
+        // Remove o esporte Handebol do vector
+        esportes.remove("Handebol");
+        
+        // Retorna o primeiro item do vector
+        System.out.println(esportes.get(2));
+        
+        // Navega nos esportes
+        for(String esporteItem: esportes)
+        {
+            System.out.print(esporteItem + " ");
+        }
     }
 }
