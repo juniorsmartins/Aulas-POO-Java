e3399b86cb72be2edc65ae02ed1b5f22f5542aca
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-02-13 09:09:02 -0400

    Treino

---
+++ Aulas-POO-Java/src/BootcampInterJavaDIO/AulaVector.java
@@ -0,0 +1,26 @@
+package BootcampInterJavaDIO;
+
+import java.util.List;
+import java.util.Vector;
+
+public class AulaVector 
+{
+
+    public static void main(String[] args) 
+    {
+        List<String> esportes = new Vector<>();
+
+        // Adiciona esportes no vector
+        esportes.add("Futebol");
+        esportes.add("Basquete");
+        esportes.add("Tênis");
+        esportes.add("Futebol Americano");
+        esportes.add("Xadrez");
+        esportes.add("Judô");
+        esportes.add("Futsal");
+        esportes.add("Vôlei");
+        esportes.add("Rugby");
+
+
+    }
+}
