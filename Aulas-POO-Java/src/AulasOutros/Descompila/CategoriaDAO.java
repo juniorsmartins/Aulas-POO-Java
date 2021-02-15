9815a65ce2b76d82130aec67f386624cc58bdf7a
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-02-15 14:16:03 -0400

    Treino

---
+++ Aulas-POO-Java/src/AulasOutros/Descompila/CategoriaDAO.java
@@ -0,0 +1,36 @@
+package AulasOutros.Descompila;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CategoriaDAO 
+{
+    private Connection con = null;
+    
+    private CategoriaDAO()
+    {
+        con = ConnectionFactory.getConnection();
+    }
+    
+    public boolean save(Categoria categoria)
+    {
+        String sql = "INSERT INTO categoria (descricao) VALUES (?)";
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, categoria.getDescricao());
+            stmt.executeUpdate();
+            return true;
+        } 
+        catch (SQLException ex) 
+        {
+            System.err.println("Erro: " + ex);
+            return false;
+        }
+        finally
+        {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+}
