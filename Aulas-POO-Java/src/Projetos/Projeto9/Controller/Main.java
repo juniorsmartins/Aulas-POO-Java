f4eb7fe76a008953f8785994459f6c785b1be28d
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-02-17 13:37:48 -0400

    Treino

--- Aulas-POO-Java/src/Projetos/Projeto9/Controller/Main.java
+++ Aulas-POO-Java/src/Projetos/Projeto9/Controller/Main.java
@@ -22,7 +22,7 @@ public class Main
         {
             System.out.println("ENTRADA DE DADOS");
             System.out.print("Nome: ");
-            nome = scan.nextLine();
+            nome = (String)scan.nextLine();
             System.out.print("Sobrenome: ");
             sobrenome = scan.nextLine();
             System.out.print("CPF: ");
