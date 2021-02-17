e50c248c30526aa6756dac86a3c515800731e203
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-02-17 10:12:52 -0400

    Treino

---
+++ Aulas-POO-Java/src/BootcampInterJavaDIO/AulaFuncaoAltaOrdem.java
@@ -0,0 +1,20 @@
+package BootcampInterJavaDIO;
+
+public class AulaFuncaoAltaOrdem 
+{
+    public static void main(String[] args) 
+    {
+        Calculo soma = (a, b) -> a + b;
+        System.out.println(executarOperacao(soma, 1, 3));
+    }
+    
+    public static int executarOperacao(Calculo calculo, int a, int b)
+    {
+        return calculo.calcular(a, b);
+    }
+}
+
+interface Calculo
+{
+    public int calcular(int a, int b);
+}
