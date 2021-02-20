50475a31235bf0ee8aba17df80977209b29111cf
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-02-20 15:29:35 -0400

    Treino

--- Aulas-POO-Java/src/Projetos/Projeto9/ModelsPersonas/Time.java
+++ Aulas-POO-Java/src/Projetos/Projeto9/ModelsPersonas/Time.java
@@ -1,6 +1,104 @@
 package Projetos.Projeto9.ModelsPersonas;
 
-public class Time 
+public class Time extends Juridica implements InterfacePessoa
 {
+    // Atributos de Classe
+    public static int totalCadastrosTime;
     
+    // Métodos Construtores
+    public Time()
+    {
+        //TODO
+    }
+    
+    // Métodos Getters e Setters de Classe
+    public static int getTotalCadastrosTime()
+    {
+        return Time.totalCadastrosTime;
+    }
+    public static void setTotalCadastrosTime()
+    {
+        Time.totalCadastrosTime += 1;
+    }
+
+    // Implementações da Interface Pessoa
+    @Override
+    public int verId()
+    {
+        return this.getId();
+    }
+    @Override
+    public String verPaisOrigem()
+    {
+        return this.getPaisOrigem();
+    }
+    @Override
+    public String verDataCadastro()
+    {
+        return this.getDataCadastro();
+    }
+    @Override
+    public String verNome()
+    {
+        return this.getRazaoSocial();
+    }
+    @Override
+    public String verSobrenome()
+    {
+        return this.getNomeFantasia();
+    }
+    @Override
+    public String verDocumento()
+    {
+        return this.getCnpj();
+    }
+    @Override
+    public String verTime()
+    {
+        return this.getTime();
+    }
+    @Override
+    public void verTudo()
+    {
+        System.out.println("\nID: " + this.verId());
+        System.out.println("Razão Social: " + this.verNome());
+        System.out.println("Nome Fantasia: " + this.verSobrenome());
+        System.out.println("CNPJ: " + this.verDocumento());
+        System.out.println("País: " + this.verPaisOrigem());
+        System.out.println("Time: " + this.verTime());
+    }
+
+    @Override
+    public void gerarId()
+    {
+        this.setId();
+        Pessoa.setTotalCadastrosPessoa();
+        Juridica.setTotalCadastrosJuridica();
+        Time.setTotalCadastrosTime();
+    }
+    @Override
+    public void editarPaisOrigem(String pais)
+    {
+        this.setPaisOrigem(pais);
+    }
+    @Override
+    public void editarNome(String nome)
+    {
+        this.setRazaoSocial(nome);
+    }
+    @Override
+    public void editarSobrenome(String sobre)
+    {
+        this.setNomeFantasia(sobre);
+    }
+    @Override
+    public void editarDocumento(String doc)
+    {
+        this.setCnpj(doc);
+    }
+    @Override
+    public void editarTime(String time)
+    {
+        this.setTime(time);
+    }
 }
