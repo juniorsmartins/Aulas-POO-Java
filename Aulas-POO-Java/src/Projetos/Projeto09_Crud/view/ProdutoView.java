484dd7a1e82e0d5f986ddb509d3e372280aac4f0
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-03-01 14:57:54 -0400

    Treino

--- Aulas-POO-Java/src/Projetos/Projeto09_Crud/view/ProdutoView.java
+++ Aulas-POO-Java/src/Projetos/Projeto09_Crud/view/ProdutoView.java
@@ -1,35 +1,241 @@
 package Projetos.Projeto09_Crud.view;
 
+import Projetos.Projeto09_Crud.dao.ProdutoDAO;
+import Projetos.Projeto09_Crud.model.Produto;
+import Projetos.Projeto09_Crud.table.ProdutoTableModel;
+import javax.swing.JOptionPane;
+
 public class ProdutoView extends javax.swing.JFrame {
 
+    Produto pro = new Produto();
+    ProdutoDAO prd = new ProdutoDAO();
+    
     public ProdutoView() {
         initComponents();
         setLocationRelativeTo(null);
-
+        tbProduto.setModel(new ProdutoTableModel(new ProdutoDAO().listarTodos()));
+        btExcluir.setEnabled(false);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        tfCodigo = new javax.swing.JTextField();
+        tfDescricao = new javax.swing.JTextField();
+        tfPreco = new javax.swing.JTextField();
+        btSalvar = new javax.swing.JButton();
+        btExcluir = new javax.swing.JButton();
+        btLimpar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbProduto = new javax.swing.JTable();
+        jLabel4 = new javax.swing.JLabel();
+        tfPesquisarDescricao = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Produto");
         setResizable(false);
+
+        jLabel1.setText("Código:");
+
+        jLabel2.setText("Descrição:");
+
+        jLabel3.setText("Preço:");
+
+        tfCodigo.setEditable(false);
+        tfCodigo.setBackground(new java.awt.Color(255, 204, 204));
+        tfCodigo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tfCodigoActionPerformed(evt);
+            }
+        });
+
+        btSalvar.setText("Salvar");
+        btSalvar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btSalvarActionPerformed(evt);
+            }
+        });
+
+        btExcluir.setText("Excluir");
+        btExcluir.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btExcluirMouseClicked(evt);
+            }
+        });
+
+        btLimpar.setText("Limpar");
+        btLimpar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btLimparMouseClicked(evt);
+            }
+        });
+
+        tbProduto.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+
+            }
+        ));
+        tbProduto.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tbProdutoMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tbProduto);
+
+        jLabel4.setText("Pesquisar (Descrição):");
+
+        tfPesquisarDescricao.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                tfPesquisarDescricaoKeyPressed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(53, 53, 53)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1139, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(tfCodigo)
+                            .addComponent(tfDescricao)
+                            .addComponent(tfPreco)
+                            .addComponent(tfPesquisarDescricao, javax.swing.GroupLayout.DEFAULT_SIZE, 263, Short.MAX_VALUE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btLimpar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btExcluir)
+                        .addGap(18, 18, 18)
+                        .addComponent(btSalvar)))
+                .addContainerGap(60, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(53, 53, 53)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addComponent(tfCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(tfDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(24, 24, 24)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(tfPreco, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btSalvar)
+                    .addComponent(btExcluir)
+                    .addComponent(btLimpar))
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(tfPesquisarDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void tfCodigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfCodigoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tfCodigoActionPerformed
+
+    private void btSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btSalvarActionPerformed
+        if(tfDescricao.getText().equals("") || tfPreco.getText().equals(""))
+        {
+            JOptionPane.showMessageDialog(null, "Há campos em branco!", "Produto - Aviso", JOptionPane.WARNING_MESSAGE);
+        }
+        else
+        {
+            if(tfCodigo.getText().equals(""))
+            {
+                pro.setDescricao_produto(tfDescricao.getText());
+                pro.setPreco_produto(Double.parseDouble(tfPreco.getText()));
+                prd.inserir(pro);
+            }
+            else
+            {
+                pro.setDescricao_produto(tfDescricao.getText());
+                pro.setPreco_produto(Double.parseDouble(tfPreco.getText()));
+                pro.setCodigo_produto(Integer.parseInt(tfCodigo.getText()));
+                prd.alterar(pro);
+            }             
+        }
+        tbProduto.setModel(new ProdutoTableModel(new ProdutoDAO().listarTodos()));
+        tfCodigo.setText("");
+        tfDescricao.setText("");
+        tfPreco.setText("");
+        tfPesquisarDescricao.setText("");
+    }//GEN-LAST:event_btSalvarActionPerformed
+
+    private void tbProdutoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbProdutoMouseClicked
+        tfCodigo.setText(tbProduto.getValueAt(tbProduto.getSelectedRow(), ProdutoTableModel.COL_CODIGO_PRODUTO).toString());
+        tfDescricao.setText(tbProduto.getValueAt(tbProduto.getSelectedRow(), ProdutoTableModel.COL_DESCRICAO_PRODUTO).toString());
+        tfPreco.setText(tbProduto.getValueAt(tbProduto.getSelectedRow(), ProdutoTableModel.COL_PRECO_PRODUTO).toString());
+        btExcluir.setEnabled(true);
+    }//GEN-LAST:event_tbProdutoMouseClicked
+
+    private void btLimparMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btLimparMouseClicked
+        tbProduto.setModel(new ProdutoTableModel(new ProdutoDAO().listarTodos()));
+        tfCodigo.setText("");
+        tfDescricao.setText("");
+        tfPreco.setText("");
+        tfPesquisarDescricao.setText("");
+    }//GEN-LAST:event_btLimparMouseClicked
+
+    private void btExcluirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btExcluirMouseClicked
+        int escolha = JOptionPane.showConfirmDialog(null, "Quer escluir?", "Produto - Excluir", JOptionPane.YES_NO_OPTION);
+        if(escolha == 0)
+        {
+            int codigo = Integer.parseInt(tfCodigo.getText());
+            prd.excluir(codigo);
+            tbProduto.setModel(new ProdutoTableModel(new ProdutoDAO().listarTodos()));
+            tfCodigo.setText("");
+            tfDescricao.setText("");
+            tfPreco.setText("");
+            tfPesquisarDescricao.setText("");            
+        }
+    }//GEN-LAST:event_btExcluirMouseClicked
+
+    private void tfPesquisarDescricaoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tfPesquisarDescricaoKeyPressed
+        String pesquisa = tfPesquisarDescricao.getText();
+        tbProduto.setModel(new ProdutoTableModel(new ProdutoDAO().listarTodosDescricao(pesquisa)));
+    }//GEN-LAST:event_tfPesquisarDescricaoKeyPressed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btExcluir;
+    private javax.swing.JButton btLimpar;
+    private javax.swing.JButton btSalvar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tbProduto;
+    private javax.swing.JTextField tfCodigo;
+    private javax.swing.JTextField tfDescricao;
+    private javax.swing.JTextField tfPesquisarDescricao;
+    private javax.swing.JTextField tfPreco;
     // End of variables declaration//GEN-END:variables
 }
