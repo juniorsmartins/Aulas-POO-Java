a42e8d4fd62d05f5c061a18a72069e873d67def7
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-03-01 09:37:25 -0400

    Treino

--- Aulas-POO-Java/src/Projetos/Projeto09_Crud/dao/ConnectionFactory.java
+++ Aulas-POO-Java/src/Projetos/Projeto09_Crud/dao/ConnectionFactory.java
@@ -1,6 +1,20 @@
 package Projetos.Projeto09_Crud.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory 
 {
-    
+    public Connection getConexao()
+    {
+        try
+        {
+            return DriverManager.getConnection("jdbc:mysql://localhost/mercado", "root", "!Jrs957895");
+        }
+        catch (SQLException erro)
+        {
+            throw new RuntimeException("Erro 1: " + erro);
+        }
+    }
 }
--- Aulas-POO-Java/src/Projetos/Projeto09_Crud/dao/ProdutoDAO.java
+++ Aulas-POO-Java/src/Projetos/Projeto09_Crud/dao/ProdutoDAO.java
@@ -1,6 +1,119 @@
 package Projetos.Projeto09_Crud.dao;
 
+import Projetos.Projeto09_Crud.model.Produto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class ProdutoDAO 
 {
+    private Connection conn;
+    private PreparedStatement stmt;
+    private Statement st;
+    private ResultSet rs;
+    private ArrayList<Produto> lista = new ArrayList<>();
+
+    public ProdutoDAO()
+    {
+        conn = new ConnectionFactory().getConexao();
+    }
     
+    public void inserir(Produto produto)
+    {
+        String sql = "INSERT INTO produto (descricao_produto, preco_produto) VALUES (?, ?)";
+        try
+        {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, produto.getDescricao_produto());
+            stmt.setDouble(2, produto.getPreco_produto());
+            stmt.execute();
+            stmt.close();
+        }
+        catch(SQLException erro)
+        {
+            throw new RuntimeException("Erro 2: " + erro);
+        }
+    }
+
+    public void alterar(Produto produto)
+    {
+        String sql = "UPDATE produto SET descricao_produto = ?, preco_produto = ? WHERE codigo_produto = ?";
+        try
+        {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, produto.getDescricao_produto());
+            stmt.setDouble(2, produto.getPreco_produto());
+            stmt.setInt(3, produto.getCodigo_produto());
+            stmt.execute();
+            stmt.close();
+        }
+        catch(SQLException err)
+        {
+            throw new RuntimeException("Erro 3: " + err);
+        }
+    }
+
+    public void excluir(int valor)
+    {
+        String sql = "DELETE FROM produto WHERE codigo_produto = " + valor;
+        try
+        {
+            st = conn.createStatement();
+            st.execute(sql);
+            st.close();
+        }
+        catch(SQLException er)
+        {
+            throw new RuntimeException("Erro 4: " + er);
+        }
+    }
+    
+    public ArrayList<Produto> listarTodos()
+    {
+        String sql = "SELECT * FROM produto";
+        try
+        {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+            while(rs.next())
+            {
+                Produto produto = new Produto();
+                produto.setCodigo_produto(rs.getInt("codigo_produto"));
+                produto.setDescricao_produto(rs.getString("descricao_produto"));
+                produto.setPreco_produto(rs.getDouble("preco_produto"));
+                lista.add(produto);
+            }
+        }
+        catch(SQLException e)
+        {
+            throw new RuntimeException("Erro 5: " + e);
+        }
+        return lista;
+    }
+
+    public ArrayList<Produto> listarTodosDescricao(String valor)
+    {
+        String sql = "SELECT * FROM produto WHERE descricao_produto LIKE '%" + valor + "%' ";
+        try
+        {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+            while(rs.next())
+            {
+                Produto produto = new Produto();
+                produto.setCodigo_produto(rs.getInt("codigo_produto"));
+                produto.setDescricao_produto(rs.getString("descricao_produto"));
+                produto.setPreco_produto(rs.getDouble("preco_produto"));
+                lista.add(produto);
+            }
+        }
+        catch(SQLException ero)
+        {
+            throw new RuntimeException("Erro 6: " + ero);
+        }
+        return lista;
+    }
 }
